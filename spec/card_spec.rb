838eaa30e5d7b8ca00416d2b085a5d22fb2e8a2f
App Academy Student <student@appacademy.io>
2013-08-30 11:28:54 -0400

    adds card and deck specs

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,1 +1,22 @@
 require 'card.rb'
+
+describe Card do
+  subject(:card) { Card.new(4, 's') }
+
+  it "has a suit" do
+    expect(card.suit).to eq('s')
+  end
+
+  it "has a value" do
+    expect(card.rank).to eq('4')
+  end
+
+  it "renders itself" do
+    expect(card.render).to eq('4♠')
+  end
+
+  it "renders face cards" do
+    card.rank = 13
+    expect(card.render).to eq('K♠')
+  end
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,1 +1,28 @@
 require 'deck.rb'
+
+describe Deck do
+  subject(:deck) { Deck.new }
+  its(:cards.length) { should eq 52 }
+
+  describe "#draw" do
+    it "draws a card" do
+      expect(deck.draw).to be_a(Card)
+    end
+
+    it "draws randomly" do
+      first = deck.draw
+      expect(deck.draw).to not_be(first)
+    end
+
+    it "removes the card from deck" do
+      deck.draw
+      expect(deck.cards.length).to eq(51)
+    end
+  end
+
+
+  it "decks aren't identical" do
+    card = Deck.new.draw
+    expect(deck.draw.value).to not_be(card.value)
+  end
+end
