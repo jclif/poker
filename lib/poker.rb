01372097c56345eefbf5c842ae2738801e54b9fe
= <=>
2013-08-30 16:05:15 -0400

    adds specs for poker method #beats?

--- lib/poker.rb
+++ lib/poker.rb
@@ -14,6 +14,14 @@ class Poker
   end
 
   def beats?(first_hand, second_hand)
+    first_type = first_hand.type
+    second_type = second_hand.type
+    first_value = HEIRARCHY.index(first_type[0])
+    second_value = HEIRARCHY.index(second_type[0])
+
+    state = first_value > second_value ? 1 : -1
     # -1 lose, 1 win, 0 draw
+
+    state
   end
 end
