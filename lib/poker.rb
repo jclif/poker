4e9232df5583a5f8a30dc17af70d77410dc52165
= <=>
2013-08-30 16:01:21 -0400

    adds specs for poker method #beats?

--- lib/poker.rb
+++ lib/poker.rb
@@ -1,11 +1,19 @@
+require './player.rb'
+require './deck.rb'
+require './hand.rb'
+require './card.rb'
+
 class Poker
   HEIRARCHY = [:highcard, :pair, :twopair, :threekind, :straight, :flush, :house, :fourkind, :straightflush]
 
   def initialize
+    @players = [Player.new, Player.new]
   end
 
   def play
   end
 
-
+  def beats?(first_hand, second_hand)
+    # -1 lose, 1 win, 0 draw
+  end
 end
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -21,14 +21,74 @@ describe Poker do
       expect(poker.beats?(hand1, hand2)).to eq(-1)
     end
 
-    it "returns -1 pair vs pair with A"
+    it "returns -1 pair vs pair with A" do
+      hand1.cards = [Card.new(2,"d"),
+                     Card.new(3,"d"),
+                     Card.new(3,"d"),
+                     Card.new(5,"h"),
+                     Card.new(9,"d"),]
+      hand2.cards = [Card.new(3,"d"),
+                     Card.new(3,"d"),
+                     Card.new(4,"d"),
+                     Card.new(5,"h"),
+                     Card.new(14,"d"),]
+      expect(poker.beats?(hand1, hand2)).to eq(-1)
+    end
 
-    it "returns 1 3ofkind vs 2pair"
+    it "returns 1 3ofkind vs 2pair" do
+      hand1.cards = [Card.new(2,"d"),
+                     Card.new(2,"d"),
+                     Card.new(2,"d"),
+                     Card.new(5,"h"),
+                     Card.new(9,"d"),]
+      hand2.cards = [Card.new(3,"d"),
+                     Card.new(3,"d"),
+                     Card.new(5,"d"),
+                     Card.new(5,"h"),
+                     Card.new(9,"d"),]
+      expect(poker.beats?(hand1, hand2)).to eq(-1)
+    end
 
-    it "returns 1 higher flush vs flush"
+    it "returns 1 higher flush vs flush" do
+      hand1.cards = [Card.new(2,"d"),
+                     Card.new(2,"d"),
+                     Card.new(3,"d"),
+                     Card.new(5,"d"),
+                     Card.new(11,"d"),]
+      hand2.cards = [Card.new(3,"d"),
+                     Card.new(3,"d"),
+                     Card.new(4,"d"),
+                     Card.new(5,"d"),
+                     Card.new(10,"d"),]
+      expect(poker.beats?(hand1, hand2)).to eq(1)
+    end
 
-    it "return 1 higher straight vs straight"
+    it "return 1 higher straight vs straight" do
+      hand1.cards = [Card.new(2,"d"),
+                     Card.new(3,"d"),
+                     Card.new(4,"d"),
+                     Card.new(5,"h"),
+                     Card.new(6,"d"),]
+      hand2.cards = [Card.new(14,"d"),
+                     Card.new(2,"d"),
+                     Card.new(4,"d"),
+                     Card.new(5,"h"),
+                     Card.new(3,"d"),]
+      expect(poker.beats?(hand1, hand2)).to eq(1)
+    end
 
-    it "return -1 higher straightflush vs straightflush"
+    it "return 1 higher straightflush vs straightflush" do
+      hand1.cards = [Card.new(10,"d"),
+                     Card.new(11,"d"),
+                     Card.new(12,"d"),
+                     Card.new(13,"d"),
+                     Card.new(14,"d"),]
+      hand2.cards = [Card.new(2,"d"),
+                     Card.new(3,"d"),
+                     Card.new(4,"d"),
+                     Card.new(5,"d"),
+                     Card.new(6,"d"),]
+      expect(poker.beats?(hand1, hand2)).to eq(1)
+    end
   end
 end
